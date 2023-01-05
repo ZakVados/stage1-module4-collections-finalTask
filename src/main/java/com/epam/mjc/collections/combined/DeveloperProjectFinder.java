4adfd113dbe115f91064da11826e9010dce3bc05
ZakVados <c0H7W2c3#mPZD$2Iuc4B0LbnruPGwi>
2023-01-05 14:58:49 +0200

    collection final task done

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,22 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        Comparator<String> comparator = (o1, o2) -> {
+            if (o1.length() < o2.length()) return 1;
+            else if (o1.length() > o2.length()) return -1;
+            else return 0;
+        };
+        List<String> res = new ArrayList<>();
+        PriorityQueue<String> pq = new PriorityQueue<String>(comparator);
+
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet()) {
+            if (entry.getValue().contains(developer)) pq.add(entry.getKey());
+        }
+        int pqSize = pq.size();
+        for (int i = 0; i < pqSize; i++) res.add(pq.poll());
+        return res;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,20 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> res = new HashMap<Integer, Set<String>>();
+        for (String inputKey : sourceMap.keySet() ) {
+            Integer entryKeyLen = inputKey.length();
+            if (res.containsKey(entryKeyLen)) {
+                res.get(entryKeyLen).add(inputKey);
+                continue;
+            }
+            Set<String> set = new HashSet<String>();
+            set.add(inputKey);
+            res.put(entryKeyLen, set);
+        }
+        return res;
     }
 }
